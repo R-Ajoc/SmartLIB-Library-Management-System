1696f7774646e9eb4ccec64010f679a17f0ba4c7
Rhazeanneth Ajoc <142295302+R-Ajoc@users.noreply.github.com>
2025-12-09 16:37:29 +0800

    Add test accounts for development in index.php
    Added test account information for development purposes.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php header("Location: views/login.php");?>
+
+<?php
+// ----------------------------------------
+// TEST ACCOUNTS FOR DEVELOPMENT PURPOSES
+// ----------------------------------------
+
+// Students
+// Username: LuvRhaz   | Password: Ajoc_2005
+// Username: Student_Rave | Password: StudRave_001
+
+// Teacher
+// Username: Teacher_Loni | Password: TeachLoni_001
+
+// Librarian
+// Username: Librarian_Ash | Password: LibAsh_001
+
+// Staff
+// Username: Staff_Allan | Password: StaffAllan_001
+
